3e120410e1e43e7f78ac219d5aa0b5c0efa25a47
Thrilly <nico.joachim@free.fr>
2018-04-02 16:08:51 +0200

    Step 4  retry 5

--- controllers/domains.js
+++ controllers/domains.js
@@ -101,13 +101,20 @@ controller.route('/domains/:domain/translations.:ext')
         },
 
         function(d, t, callback){
+            Lang.getDomainLangs(d.id, function(dl){
+                callback(null, d, t, dl);
+            });
+        },
+
+        function(d, t, dl, callback){
             Translation.getTranslationsToLangByDomain(d.id, function(tl){
                 for (var i = 0; i < t.length; i++) {
                     if (typeof tl[t[i].id] !== 'undefined') {
                         t[i].trans = tl[t[i].id];
-                        t[i].trans.PL = t[i].code;
-                    }else{
-                        t[i].trans = {"PL" : t[i].code};
+                        for (var key in dl){
+                            var idlang = dl[key];
+                            if (typeof t[i].trans[idlang] === 'undefined') t[i].trans[idlang] = t[i].code;
+                        }
                     }
 
                 }
@@ -136,8 +143,10 @@ controller.route('/domains/:domain/translations.:ext')
         function(callback) {
             if (typeof req.header('Authorization') === 'undefined'){
                 callback(401, "Authorization is required");
+                return;
             } else if (typeof req.header('Content-Type') === 'undefined' && typeof req.header('Content-Type') === 'undefined') {
                 callback(401, "Content-Type is required");
+                return;
             } else {
                 callback(null);
             }
@@ -147,8 +156,10 @@ controller.route('/domains/:domain/translations.:ext')
             Domain.getDomain(domain, function(d){
                 if (ext != "json") {
                     callback(400, 'Bad request : Extension \''+ext+'\' not available');
+                    return;
                 }else if (!d){
                     callback(404, 'Not Found : Unknow domain \''+domain+'\''); 
+                    return;
                 }else{
                     callback(null, d); 
                 }
@@ -161,10 +172,13 @@ controller.route('/domains/:domain/translations.:ext')
             User.authenticate(req.header('Authorization'), d.id, function(authDomain){
                 if (authDomain === false) {
                     callback(401, "Wrong token given");
+                    return;
                 }else if(authDomain != d.id){
                     callback(403, "Access Denied");
+                    return;
                 }else if(req.header('Content-Type') != "application/x-www-form-urlencoded" || req.header('Content-type') != "application/x-www-form-urlencoded"){
                     callback(401, "Invalid Content-Type");
+                    return;
                 }else{
                     callback(null, d);
                 }
@@ -174,8 +188,9 @@ controller.route('/domains/:domain/translations.:ext')
         function(d, callback){
             var requiredFields = ['code', 'trans'];
             for (var i = 0; i < requiredFields.length; i++) {
-                if(typeof req.body[requiredFields[i]] === 'undefined' || req.body[requiredFields[i]] == '' || req.body[requiredFields[i]] == '\\0'){
+                if(typeof req.body[requiredFields[i]] === 'undefined' || req.body[requiredFields[i]] == '' || req.body[requiredFields[i]] == '\0' || req.body[requiredFields[i]] == '\\0'){
                     callback(400, '\''+requiredFields[i]+'\' parameter is missing');
+                    return;
                 }
             }
             callback(null, d);
@@ -191,11 +206,14 @@ controller.route('/domains/:domain/translations.:ext')
                         if (regex.test(k) === false) {
                             if (reg == '') {
                                 callback(400, "No lang is registered in this domain");
+                                return;
                             }
                             callback(400, "\'trans\' need registered iso code array keys ("+reg+")");
+                            return;
                         }
-                        if (req.body.trans[k].length == 0 || req.body.trans[k] == "\0") {
+                        if (req.body.trans[k].length == 0 || req.body.trans[k] == "\0" || req.body.trans[k] == "\\0") {
                             callback(400, '\'trans\' values cannot be empty');
+                            return;
                         } 
                     }
                     callback(null, d);
@@ -215,6 +233,123 @@ controller.route('/domains/:domain/translations.:ext')
                     res.status(201).json({ code: 201, message: 'success', datas: result});
                 }else{
                     callback(400, 'SQL MESSAGE : '+result.error);
+                }
+            });
+        },
+
+    ],function(err, msg) {
+        if (err == 400) { res.status(err).json({ code: err, message: msg, data:[]})} else {res.status(err).json({ code: err, message: msg})}
+    });
+    
+});
+
+
+
+// ####################### ROUTE 5 #######################
+
+controller.route('/domains/:domain/translations/:idtrans.:ext')
+
+.put(function(req, res) {
+    var domain = req.params.domain;
+    var ext = req.params.ext;
+    var idtrans = req.params.idtrans;
+
+    async.waterfall([
+
+        function(callback) {
+            if (typeof req.header('Authorization') === 'undefined'){
+                callback(401, "Authorization is required");
+                return;
+            } else if (typeof req.header('Content-Type') === 'undefined' && typeof req.header('Content-Type') === 'undefined') {
+                callback(401, "Content-Type is required");
+                return;
+            } else {
+                callback(null);
+            }
+        },
+
+        function(callback){
+            Domain.getDomain(domain, function(d){
+                if (ext != "json") {
+                    callback(400, 'Bad request : Extension \''+ext+'\' not available');
+                    return;
+                }else if (!d){
+                    callback(404, 'Not Found : Unknow domain \''+domain+'\''); 
+                    return;
+                }else{
+                    callback(null, d); 
+                }
+            });
+        },
+
+        
+
+        function(d, callback){
+            User.authenticate(req.header('Authorization'), d.id, function(authDomain){
+                if (authDomain === false) {
+                    callback(401, "Wrong token given");
+                    return;
+                }else if(authDomain != d.id){
+                    callback(403, "Access Denied");
+                    return;
+                }else if(req.header('Content-Type') != "application/x-www-form-urlencoded" || req.header('Content-type') != "application/x-www-form-urlencoded"){
+                    callback(401, "Invalid Content-Type");
+                    return;
+                }else{
+                    callback(null, d);
+                }
+            });
+        },
+
+        function(d, callback){
+            var requiredFields = ['trans'];
+            for (var i = 0; i < requiredFields.length; i++) {
+                if(typeof req.body[requiredFields[i]] === 'undefined' || req.body[requiredFields[i]] == '' || req.body[requiredFields[i]] == '\\0'){
+                    callback(400, '\''+requiredFields[i]+'\' parameter is missing');
+                    return;
+                }
+            }
+            callback(null, d);
+        },
+
+        function(d, callback){
+            Lang.getRegexDomainLangs(d.id, function(reg){
+                if (typeof req.body.trans != "object") {
+                    callback(400, '\'trans\' need to be an array');
+                    return;
+                }else{
+                    for (var k in req.body.trans){
+                        var regex = RegExp('['+reg+']{2}');
+                        if (regex.test(k) === false) {
+                            if (reg == '') {
+                                callback(400, "No lang is registered in this domain"); 
+                                return;
+                            }
+                            callback(400, "\'trans\' need registered iso code array keys ("+reg+")");
+                            return;
+                        }
+                        if (req.body.trans[k].length == 0 || req.body.trans[k] == "\0") {
+                            callback(400, '\'trans\' values cannot be empty');
+                            return;
+                        } 
+                    }
+                    callback(null, d);
+                }
+            });
+        },
+
+        function(d, callback){
+            Lang.getDomainLangs(d.id, function(dl){
+                callback(null, d, dl);
+            });
+        },
+
+        function(d, dl, callback){
+            Translation.setTranslation(d.id, dl, idtrans, req.body.trans, function(result){
+                if (typeof result.error === 'undefined') {
+                    res.status(201).json({ code: 201, message: 'success', datas: result});
+                }else{
+                    callback(400, result.error);
                 }
             });
         },
